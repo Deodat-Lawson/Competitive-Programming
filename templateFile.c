9932898da328d93b66dffaf1c2dbb7c3594b149d
Timothy Lin <timothylinziqi@gmail.com>
2022-09-02 18:50:15 -0400

    hi

---
+++ templateFile.c
@@ -0,0 +1,12 @@
+//
+// Created by tlin56 on 9/2/22.
+//
+
+
+//This is the debugging template for
+#include <stdio.h>
+int main(){
+    int a = 0;
+    printf("Hello, World");
+    return 0;
+}
--- template_debug.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by tlin56 on 9/2/22.
-//
-
-int main(){
-
-
-    return 0;
-}
